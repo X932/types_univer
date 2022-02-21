c38b26db12cad2dacd09ce0c0fa04f1352566d79
Yusuf <66509035+X932@users.noreply.github.com>
2022-02-21 18:36:52 +0500

    added mssg patterns for authotization (#5)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const UniverOptions = require('./services/univer/config');
 const DocumentOptions = require('./services/documents/config');
+const AuthMessagePatterns = require('./services/univer/message-patterns/auth');
 const ServicesConfigs = {
   UniverOptions,
   DocumentOptions,
+  AuthMessagePatterns,
 };
 
 module.exports = { ServicesConfigs };
